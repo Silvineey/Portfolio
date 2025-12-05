ad47d7c46d058c0454ac6ebe71368e835142918c
Silvineey <lucreciasilvio@hotmail.com>
2025-12-05 16:04:35 -0300

    ajustes

--- codigo.js
+++ codigo.js
@@ -1,65 +1,45 @@
 const simbolo = document.getElementById("simbolo")
-function mudartema() {
-    const caixa = document.querySelector(".caixatema")
-    const bolinha = document.querySelector(".bolinha")
-    document.body.classList.toggle("modoescuro")
-    bolinha.classList.toggle("ativa")
-    caixa.classList.toggle("ativa")
-    if (document.body.classList.contains("modoescuro")) {
-        if (document.getElementById("eric").checked) {
-            simbolo.src = "img/designbranco.png"
-        } else {
-            simbolo.src = "img/devbranco.png"
-        }
-    } else {
-        if (document.getElementById("eric").checked) {
-            simbolo.src = "img/designpreto.png"
-        } else {
-            simbolo.src = "img/devpreto.png"
-        }
-    }
-
-}
-
 const form = document.getElementById("formulario").addEventListener("click", (e) => {
     const alvo = e.target
     if (alvo.type === "radio") {
         const id = alvo.id
-        const h1 = document.querySelector(".caixa h1")
         const design = document.getElementById("design")
         const dev = document.getElementById("dev")
+        const projetosdev=document.getElementById("projetosdev")
+        const projetosdesign=document.getElementById("projetosdesign")
 
-        if (id === "eric" && document.body.classList.contains("modoescuro")) {
-            simbolo.src = "img/designbranco.png"
-            h1.innerHTML = "Web Design"
+        if (id === "eric") {
             dev.classList.add("oculto")
+            projetosdev.classList.add("oculto")
+            setTimeout(() => {
+                design.style.display="flex"
+                projetosdesign.style.display="block"
+            }, 200);
             setTimeout(() => {
                 design.classList.remove("oculto")
-            }, 500);
-        }
-        else if (id === "eric" && !document.body.classList.contains("modoescuro")) {
-            simbolo.src = "img/designpreto.png"
-            h1.innerHTML = "Web Design"
-            dev.classList.add("oculto")
+                projetosdesign.classList.remove("oculto")
+                simbolo.src = "img/designlogo.png"
+            }, 300);
             setTimeout(() => {
-                design.classList.remove("oculto")
-            }, 500);
+            dev.style.display="none"
+            projetosdev.style.display="none"
+            }, 400);
         }
-        else if (id === "sydney" && document.body.classList.contains("modoescuro")) {
-            simbolo.src = "img/devbranco.png"
-            h1.innerHTML = "Desenvolvedor Web"
+        else if(id === "sydney" ) {
+            simbolo.src = "img/devlogo.png"
             design.classList.add("oculto")
+            projetosdesign.classList.add("oculto")
+                setTimeout(() => {
+                dev.style.display="flex"
+                projetosdev.style.display="block"
+            }, 200);
             setTimeout(() => {
                 dev.classList.remove("oculto")
-            }, 500);
-        }
-        else if (id === "sydney" && !document.body.classList.contains("modoescuro")) {
-            simbolo.src = "img/devpreto.png"
-            h1.innerHTML = "Desenvolvedor Web"
-            design.classList.add("oculto")
+                projetosdev.classList.remove("oculto")
+                simbolo.src = "img/devlogo.png"
+            }, 300);
             setTimeout(() => {
-                dev.classList.remove("oculto")
-            }, 500);
-        }
-    }
-})
+                design.style.display="none"
+                projetosdesign.style.display="none"
+            }, 400);
+        }}});
